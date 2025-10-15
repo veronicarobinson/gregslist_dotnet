26692c2f7e0e7b6f349729cae87f735515d83a04
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-15 12:51:28 -0600

    put your own junk in there

--- wwwroot/env.js
+++ wwwroot/env.js
@@ -1,4 +1,4 @@
 // TODO put your own Auth0 keys here!!!
-export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
-export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
-export const audience = 'https://jeremyisaraddude.com'
+export const domain = ''
+export const clientId = ''
+export const audience = ''

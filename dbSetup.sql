6ea85f3c7e60b153967b8ec01e0b63328555ce1a
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-15 09:23:45 -0600

    filled out env and appsettings development

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,8 +1,10 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name VARCHAR(255) COMMENT 'User Name',
-  email VARCHAR(255) UNIQUE COMMENT 'User Email',
-  picture VARCHAR(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+-- TODO don't forget to create your accounts table today!
+CREATE TABLE
+  IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) COMMENT 'User Name',
+    email VARCHAR(255) UNIQUE COMMENT 'User Email',
+    picture VARCHAR(255) COMMENT 'User Picture'
+  ) DEFAULT charset utf8mb4 COMMENT '';

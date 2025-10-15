6162fd3f5ab6737f6ee12c88b783cf96ab49806d
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-15 10:02:07 -0600

    data validation in car model

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace gregslist_api_dotnet.Models;
 
 public class Car
@@ -5,15 +7,16 @@ public class Car
   public int Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
-  public string Make { get; set; }
-  public string Model { get; set; }
-  public int Year { get; set; }
-  public int Price { get; set; }
-  public string ImgUrl { get; set; }
-  public string Description { get; set; }
+  [MinLength(3), MaxLength(50)] public string Make { get; set; }
+  [MinLength(1), MaxLength(100)] public string Model { get; set; }
+  // TODO show off year validation with setter
+  [Range(1886, 2026)] public int Year { get; set; }
+  [Range(0, 1000000)] public int Price { get; set; }
+  [Url, MaxLength(500)] public string ImgUrl { get; set; }
+  [MaxLength(500)] public string Description { get; set; }
   public string EngineType { get; set; }
-  public string Color { get; set; }
-  public int Mileage { get; set; }
+  [MaxLength(50)] public string Color { get; set; }
+  [Range(0, 1000000)] public int Mileage { get; set; }
   public bool HasCleanTitle { get; set; }
   public string CreatorId { get; set; }
 }
